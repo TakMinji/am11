054b2444eece56d9e1b3a50ac0a9cf0e8ff99564
TakMinji <tkm1238@naver.com>
2024-03-15 12:49:10 +0900

    switch 마지막 실습문제(switch문을 이용한 계산기)

--- JS/step2/js/2_switch.js
+++ JS/step2/js/2_switch.js
@@ -6,7 +6,7 @@
 // *case.1 문법 - 홀,짝 구분
 function switchGrammar(){
     var input = Number(prompt('숫자를 입력하라'));
-    switch (input % 2) {
+    switch (input % 2){
         case 0:
             alert('짝수입니다.');
             break;
@@ -29,7 +29,7 @@ function switchGrammar(){
 //  *case.2 Lotto과제를 switch로 변경
 function lotto(){
     var numbValue = prompt("입력하실 번호는?");
-    switch (numbValue) {
+    switch (numbValue){
         case "1":
             alert("1등 15억!");
             break;
@@ -41,6 +41,36 @@ function lotto(){
             break;
         default:
             alert("낙첨되었습니다.");
+            break;
+    }
+}
+
+// todo. switch문을 이용한 계산기
+// ? 첫번째 숫자와 연산자와 두번째 숫자를 입력받아 계산해주는 사칙연산기.
+function calculator(){
+    var firstNumb = prompt("첫번째 숫자를 입력하세요.");
+    var operator = prompt("사칙연산자중(+,-,*,/) 하나를 입력하세요.");
+    var lastNumb = prompt("두번째 숫자를 입력하세요.");    
+
+    // ? 형변환
+    firstNumb = Number(firstNumb);
+    lastNumb = Number(lastNumb);
+
+    switch () {
+        case :
+            alert(firstNumb?lastNumb);
+            break;
+        case :
+            alert(firstNumb?lastNumb);
+            break;
+        case :
+            alert(firstNumb?lastNumb);
+            break;
+        case :
+            alert(firstNumb?lastNumb);
+            break;
+        default:
+            alert("연산이 불가능한 값이 입력되었습니다.");
             break;
     }
 }
