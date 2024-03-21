74d9225989785b9487672ba1526e1e9d5ee2941b
TakMinji <tkm1238@naver.com>
2024-03-21 11:25:18 +0900

    while과제 해설

--- JS/step3/js/1_whileHW.js
+++ JS/step3/js/1_whileHW.js
@@ -1,15 +1,21 @@
 //todo. end가 입력될 때 까지 숫자를 계속해서 입력받아 입력값 합을 출력하고, prompt에는 몇번 수행 했는가에 대한 "x 번째 입력" 이 표시되야함. 
 function sumNumb(){
-    var sum = 0;
-    var num = 1;
-    var count = 0;
-    while(num != 0) {
-         num = Number(prompt("숫자를 입력해 주세요(0 = 종료)"));
-        sum += num;         
-        count++;  
-        console.log(count, num, sum);     
-    }    
+    // ? 선언부
+    var i = 1; //? 카운트 변수
+    var result = 0; //? 결과를 담을 변수
+    while(true){
 
-    document.write( sum + "번째 입력");
+        var numb = window.prompt(i+"번째 입력"); //? 무한 루프로 입력받기.
+        if(numb=="end"){ //? end 라는 값과 일치하면
+            alert('입력이 종료되었습니다.');
+            break; //? 루프 중단 및 종료alert 출력
+        }else if(numb == "" || numb == " "){
+            alert("입력된 값이 잘못되었습니다. 다시 입력하세요.");
+        }
+        result += parseInt(numb); //? 결과변수에 형변환후 더해서 저장.
+        document.write("현재까지의 총합 : " + result + "<br>"); //? 저장된 result 값을 출력.
+        i++; //? 수행할 때마다 카운트 중가.
+    }
+
 }
 // 1 누르면 1번째 입력 2누르면 2번째 입력..end 입력하면 입력이 종료되었습니다.
