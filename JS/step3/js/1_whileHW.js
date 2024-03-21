f6330e582e11ea4002fba69a0d744e0bc8b4ad09
TakMinji <tkm1238@naver.com>
2024-03-21 11:16:17 +0900

    for과제 완료.

--- JS/step3/js/1_whileHW.js
+++ JS/step3/js/1_whileHW.js
@@ -1,12 +1,15 @@
 //todo. end가 입력될 때 까지 숫자를 계속해서 입력받아 입력값 합을 출력하고, prompt에는 몇번 수행 했는가에 대한 "x 번째 입력" 이 표시되야함. 
 function sumNumb(){
+    var sum = 0;
+    var num = 1;
+    var count = 0;
+    while(num != 0) {
+         num = Number(prompt("숫자를 입력해 주세요(0 = 종료)"));
+        sum += num;         
+        count++;  
+        console.log(count, num, sum);     
+    }    
 
-
-    // ? 선언부
-    while(true){
-        
-    }
-
+    document.write( sum + "번째 입력");
 }
-
 // 1 누르면 1번째 입력 2누르면 2번째 입력..end 입력하면 입력이 종료되었습니다.
--- JS/step3/js/3_forHW.js
+++ JS/step3/js/3_forHW.js
@@ -1,6 +1,22 @@
 //todo.1 구구단 숫자를 입력받아 출력하는 for문.
 // ? 어떤 숫자를 입력받던 해당 "구단(x1~x9)"을 출력하면 됨
+function gugudan(){
+    var num = prompt("구구단 숫자를 입력");
+    for(var i = 1; i <= 9; i++){
+        var result = num + "x" + i + "=" + num*i + "<br>"
+        document.write(result);
 
+    }
+}
 
 // todo.2 배열의 총 합을 구하기. var data = [10,20,30,40,50];
 // ? 해당 배열의 총합은 = 150 메시지가 배열의 총합식에 의거하여 출력하면 됨.
+function arraySum(){
+    var data = [10, 20, 30, 40, 50];
+    var sum = 0;
+    // 숫자로 왔다갔다 하는 아이라서 0으로 선언 초기화한 것.
+    for(var i=0; i < data.length; i++){
+        sum += data[i];
+    }
+    document.write("해당 배열의 총합은 = " + sum);
+}
