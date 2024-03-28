d10184d5ea5cbf1a359b061cf496b735242a621a
TakMinji <tkm1238@naver.com>
2024-03-28 12:30:12 +0900

    Todo. 과제해오기(4월1일까지)

---
+++ JQuery/js/6_foldTODO.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    gallery2(0,200,200);
+    // ? 초기 set - 열수, 너비, 높이
+    //  todo. 열 버튼 click시 해당 열로 배치
+    //  todo. 열 버튼 click시 해당 열로 배치
+    //  todo. 열 버튼 click시 해당 열로 배치
+    //  todo. 열 버튼 click시 해당 열로 배치
+    //  todo. 열 버튼 click시 해당 열로 배치
+    //  todo. 열 버튼 click시 해당 열로 배치
+    //  todo. 열 버튼 click시 해당 열로 배치
+    //  todo. 열 버튼 click시 해당 열로 배치
+    //  todo. 열 버튼 click시 해당 열로 배치
+    //  todo. 열 버튼 click시 해당 열로 배치
+
+    // ! replace - jQuery 함수 검색해서 한번의 호출로 n개의 버튼 소화하기.
+});
+function gallery2(count,imgWidth,imgHeight){
+
+}
+// gallery2 -> 이 함수는 한개여야함.
+// 버튼을 클릭했을 때 해당 열과 사이즈만 정확히 전달하면 됨.
