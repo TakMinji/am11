fcbcef3f33c0b3ba17db7c0b3d60cf277d5fe590
TakMinji <tkm1238@naver.com>
2024-04-01 11:31:50 +0900

    attrControl

---
+++ JQuery/js/12_attrControl.js
@@ -0,0 +1,23 @@
+var currentIndex = 0;
+var logos = ["logo_01.jpg","logo_02.jpg","logo_03.jpg","logo_04.jpg","logo_05.jpg","logo_06.jpg","logo_07.jpg","logo_08.jpg"];
+var $banner = null;
+
+$(document).ready(function(){
+    init();
+    setInterval(swap,1000);
+});
+function init(){
+    $banner = $(".banner");
+}
+function swap(){
+    var imgName = "images/" + logos[currentIndex];
+    // 경로 포함한 파일명 + 배열[변수 0]
+    $banner.attr("src",imgName); // ? src값을 불러와라 / src값을 imgName으로 변경해라.
+    currentIndex++;
+    reset();
+}
+function reset(){
+    if(currentIndex == logos.length){
+        currentIndex = 0;
+    }
+}
