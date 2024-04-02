7e54e1ebf8741d289040dfcbc870302b9f7f4b7a
TakMinji <tkm1238@naver.com>
2024-04-02 11:55:49 +0900

    fish Race Game-winnerList배열에 담긴여부 확인하는 조건문

---
+++ JQuery/js/15_finJquery.js
@@ -0,0 +1,80 @@
+var $fishList = null;
+var timerID = -1;
+var $info = null;
+var goalLine = 0;
+var $fishWidth = 0;
+
+$(function(){
+    init();
+    fishStarPosition();
+    initEvent();
+});
+
+function init(){
+    $fishList = $(".fish");
+    $info = $("#info");
+    $fishWidth = $(".fish img").width();
+    goalLine = $("#bar").position().left - $fishWidth;
+    console.log("1. 대상선언완료.");
+}
+
+function fishStarPosition(){
+    for(var i = 0; i < $fishList.length; i++){
+        var $fish = $fishList.eq(i);
+        $fish.css({
+            left : 0,
+            top: 100 + (i * 150)
+        });
+    }
+    console.log("2. 초기위치설정완료.");
+}
+function initEvent(){
+    $("#start").click(startGame);
+}
+function startGame(){
+    if(timerID == -1){
+        timerID - setInterval(function(){
+            updateFishPosition(); // ? 물고기 움직임
+            displayFishPositionInfo(); // ? 움직인 위치 표시
+            checkGoalFish(); // ? 결승선 도달 감지
+        },200);
+        console.log("3. 게임 정상 시작.");
+    }
+}
+function updateFishPosition(){
+    for (var i = 0; i < $fishList.length; i++){
+        var $fish = $fishList.eq(i);
+        var step = Math.ceil(Math.random()*30);
+        // 30까지의 random을 뽑아내는 공식
+        var newLeft = $fish.position().left + step;
+        $fish.css("left", newLeft);
+    }
+    console.log("4. 물고기 움직임 정상");
+}
+function displayFishPositionInfo(){
+    var info ="";
+    for(var i = 0; i < $fishList.length; i++){
+        var $fish = $fishList.eq(i);
+        info += i + "번 물고기 : " + $fish.position().left + "px <br>";
+    }
+    $info.html(info);
+    console.log("5. 물고기 위치 표시 정상");
+}
+function checkGoalFish(){
+    var winnerList = [];
+    for(var i = 0; i < $fishList.length; i++){
+        var fishCurrentPosition = $fishList.eq(i).position().left;
+        if(fishCurrentPosition >= goalLine){
+            winnerList.push({
+                index: (i), position: fishCurrentPosition
+            });
+            // winnerList - 빈배열에 / push - 밀어넣는다.
+            // 저장함수 : push(서버에 밀어넣는다.서버가 없어도 쓸 수 있음) 
+            // pull(데이터를 당긴다.서버가 있어야만 쓸 수 있음)
+            console.log("6.  데이터 저장 성공");
+        }
+    }
+    if(winnerList.length>0){
+        
+    }
+}
--- JQuery/js/6_foldTODO.js
+++ JQuery/js/6_foldTODO.js
@@ -1,21 +1,53 @@
 $(document).ready(function(){
     gallery2(0,200,200);
-    // ? 초기 set - 열수, 너비, 높이
-    //  todo. 열 버튼 click시 해당 열로 배치
-    //  todo. 열 버튼 click시 해당 열로 배치
-    //  todo. 열 버튼 click시 해당 열로 배치
-    //  todo. 열 버튼 click시 해당 열로 배치
-    //  todo. 열 버튼 click시 해당 열로 배치
-    //  todo. 열 버튼 click시 해당 열로 배치
-    //  todo. 열 버튼 click시 해당 열로 배치
-    //  todo. 열 버튼 click시 해당 열로 배치
-    //  todo. 열 버튼 click시 해당 열로 배치
-    //  todo. 열 버튼 click시 해당 열로 배치
-
-    // ! replace - jQuery 함수 검색해서 한번의 호출로 n개의 버튼 소화하기.
+    // ? 초기 set - 열수,너비,높이
+    function gallery2(count, imgWidth, imgHeight){
+        arrangeImages(count, imgWidth, imgHeight);
+    }
 });
-function gallery2(count,imgWidth,imgHeight){
 
+    // todo. 1열 버튼 click시 해당 열로 배치.
+    $(".1way").click(function(){
+        var imgLength = img.length;
+        var imgWidth = img.width();
+        var imgHeight = img.height();
+        var columnCount = parseInt($(this).attr('class').charAt(0)); // 버튼의 클래스에서 열의 수 추출
+        var imgWidth = 1000 / columnCount; // 이미지 너비 계산
+        var imgHeight = imgWidth; // 이미지 높이는 너비와 같도록 설정
+        gallery2(columnCount, imgWidth, imgHeight); // 이미지 배치 함수 호출
+    });
+    // todo. 2열 버튼 click시 해당 열로 배치.
+    // todo. 3열 버튼 click시 해당 열로 배치.
+    // todo. 4열 버튼 click시 해당 열로 배치.
+    // todo. 5열 버튼 click시 해당 열로 배치.
+    // todo. 6열 버튼 click시 해당 열로 배치.
+    // todo. 7열 버튼 click시 해당 열로 배치.
+    // todo. 8열 버튼 click시 해당 열로 배치.
+    // todo. 9열 버튼 click시 해당 열로 배치.
+    // todo. 10열 버튼 click시 해당 열로 배치.
+
+    // ! replace - JQuery 함수 검색해서 한번의 호출로 n개의 버튼 소화하기.
+
+ function gallery2(columnCount, imgWidth, imgHeight) {
+        var $imgs = $('#imgContainer img');
+        var totalImgs = $imgs.length;
+        var currentRow = 0;
+        var currentColumn = 0;
+
+        $imgs.each(function(index) {
+            $(this).css({
+                'left': currentColumn * imgWidth,
+                'top': currentRow * imgHeight,
+                'width': imgWidth,
+                'height': imgHeight
+            });
+            
+            currentColumn++;
+            if (currentColumn >= columnCount) {
+                currentColumn = 0;
+                currentRow++;
+            }
+        });
 }
 // gallery2 -> 이 함수는 한개여야함.
 // 버튼을 클릭했을 때 해당 열과 사이즈만 정확히 전달하면 됨.
