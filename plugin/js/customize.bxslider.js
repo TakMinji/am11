59dffd6bac7ca19f9a59534a622549b806afa1ba
TakMinji <tkm1238@naver.com>
2024-04-03 11:38:07 +0900

    speed&ticker

--- plugin/js/customize.bxslider.js
+++ plugin/js/customize.bxslider.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-    $('.bxSlider').bxSlider();
+    $('.bxSlider').bxSlider({
+      mode : 'horizontal',
+      speed : 750,
+      ticker : false, 
+      tickerHover : false
+      //  speed, ticker, tickerHover 밀접한 관계
+    });
   });
