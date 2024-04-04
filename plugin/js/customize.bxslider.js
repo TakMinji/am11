46d0ff29fb71f13670fca3f47690df9a11afa456
TakMinji <tkm1238@naver.com>
2024-04-04 12:21:30 +0900

    bxslider 끝 / 정리내용은 선생님 git 참고하기

--- plugin/js/customize.bxslider.js
+++ plugin/js/customize.bxslider.js
@@ -27,8 +27,8 @@ $(document).ready(function(){
       pager: true, //? default: true. pager 켜기/끄기
       pagerType: 'full', //? default: full.(circle). short: count로 변경
       //? pagerShortSeparator: '/'  default: / -> . 식별자변경.
-      //? pagerSelector: '.newPager' default: ''. 공간 생성 및 class 부여 하면 pager 뼈대제공.
-      pagerCustom: '.customPager',
+      pagerSelector: '.newPager', // ? default: ''. 공간 생성 및 class 부여 하면 pager 뼈대제공.
+      // pagerCustom: '.customPager',
       
       // *Controler
       controls: true,
@@ -36,6 +36,28 @@ $(document).ready(function(){
       // prevSelector: '.btn_prev',
       // nextText: '다음',
       // prevText: '이전'
+      
+
+      // *auto
+      auto: true, //? default: false. slide 자동전환 켜기. false로 설정시 autoControls에서 작동가능.
+      autoControls: true, //? default: false. 시각장애인들을 배려 하는것.
+      autoControlsSelector: '.newAutoControls', //? auto controler 뼈대 제공.
+      startText: '시작', //? 생성된 뼈대 안에 text 변경
+      stopText: '중지',
+      autoStart: true, //? default: true. (false: autoControls로 시작)
+      autoDelay: 1500,
+      pause: 4000,
+      autoHover: true,
+      stopAutoOnClick: true,
+      autoControlsCombine: true,
+
+      // * carousel
+      maxSlides: 2, //? default: 1. 최대 노출 슬라이드 장수.
+      minSlides: 1, //? default: 1. 최소 노출 슬라이드 장수.
+      slideWidth: 640,
+      slideMargin: 20,
+      moveSlides: 1, // ? 전환시 이동할 carousel content 수
+      shirinkItems: true
 
     });
   });
